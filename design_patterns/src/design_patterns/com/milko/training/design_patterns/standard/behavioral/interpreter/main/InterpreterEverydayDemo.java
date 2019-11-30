673f0451b35b7b8f3d0331687b72a26355ab7533
Milko Galev <milko.galev@effortel.com>
2019-11-30 16:43:33 +0200

    InterpreterEverydayDemo

---
+++ design_patterns/src/design_patterns/com/milko/training/design_patterns/standard/behavioral/interpreter/main/InterpreterEverydayDemo.java
@@ -0,0 +1,20 @@
+package design_patterns.com.milko.training.design_patterns.standard.behavioral.interpreter.main;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InterpreterEverydayDemo {
+
+	public static void main(String[] args) {
+		String input = "Lions, and tigers, and bears! Oh, my!";
+		
+		Pattern p = Pattern.compile("(lion|cat|dog|wolf|bear|human|tiger|liger|Lion)");
+		Matcher m = p.matcher(input);
+		
+		while (m.find()) {
+			System.out.println("Found a " + m.group() + ".");
+		}
+
+	}
+
+}
