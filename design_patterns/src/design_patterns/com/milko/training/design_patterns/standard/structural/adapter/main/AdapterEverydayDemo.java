232b9e54a1488b5f744cd29eec9dc838557d742c
Milko Galev <milko.galev@effortel.com>
2019-12-07 19:35:54 +0200

    Structural - Adapter -> AdapterEverydayDemo

---
+++ design_patterns/src/design_patterns/com/milko/training/design_patterns/standard/structural/adapter/main/AdapterEverydayDemo.java
@@ -0,0 +1,15 @@
+package design_patterns.com.milko.training.design_patterns.standard.structural.adapter.main;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AdapterEverydayDemo {
+
+	public static void main(String[] args) {
+		Integer[] arrayOfInts = new Integer[] { 42, 43, 44 };
+		List<Integer> listOfInts = Arrays.asList(arrayOfInts);
+		System.out.println(arrayOfInts);
+		System.out.println(listOfInts);
+	}
+
+}
